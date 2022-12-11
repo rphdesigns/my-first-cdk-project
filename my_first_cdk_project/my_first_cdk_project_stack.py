8094ccdc6fc99b9cf0857453166f1f7574405df5
Michael Heesch <development@rphdesigns.com>
2022-12-11 15:25:50 -0500

    initial commit

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_s3 as _s3,
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -10,10 +11,10 @@ class MyFirstCdkProjectStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "MyFirstCdkProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        _s3.Bucket(
+            self,
+            "MyBucketId",
+            bucket_name="myfirstcdkproject2727",
+            versioned=True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+        )
